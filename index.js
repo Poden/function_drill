cca4beeb4f77933abf2775fd4ce1345d67f1f160
Poden <56049210+Poden@users.noreply.github.com>
2020-01-29 19:30:45 -0600

    function-drills-1

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-
-function createGreeting(name, age){
-const yearOfBirth
-
-  return (`Hi, my name is ${name} and Im ${age} years old`); 
+function getYearOfBirth(age){
+  return 2020 - age;
 }
 
+function createGreeting(name, age) {
+  if (name === undefined || age === undefined ){
+    throw new Error('values entered are undefined')
+  }
+  if (age < 0) {
+    throw new Error('Age has to be positive');
+  }
+  if (typeof age !== 'number'){
+    throw new Error('age must be a number');
+  }
+  const yob = getYearOfBirth(age);
+  return ` Hi my name is ${name} and im ${age} years old.
+  I was born in ${yob}.`;
+}
 
-const greeting1 = createGreeting();
-console.log(greeting1);
+try{
+  const greeting1 = createGreeting('nahomi', 22);
+  console.log(greeting1);
+} catch(e){
+  console.error(e.message);
+}
